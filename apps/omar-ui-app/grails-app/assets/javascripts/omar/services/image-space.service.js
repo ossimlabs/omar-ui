1bc78b3db35d2eebf1fb5c6bc1e2e86ec6f4e44a
knaquin <knaquin@radiantblue.com>
2017-04-06 15:55:05 -0400

    Adding new docs URL.

--- apps/omar-ui-app/grails-app/assets/javascripts/omar/services/image-space.service.js
+++ apps/omar-ui-app/grails-app/assets/javascripts/omar/services/image-space.service.js
@@ -278,7 +278,7 @@
 
               stateService.navStateUpdate({
                 titleLeft: imageIdText + " <br> " + acquisitionDateText,
-                userGuideUrl: "user-guide/image-space/"
+                userGuideUrl: "omar-ui/docs/user-guide/omar-ui/#image-space"
               });
             });
 
